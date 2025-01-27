6d94561cc60d004685d2ed12ea8924f050d37da4
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-27 12:33:10 +0100

    add: es. 8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const MarcoLanci = students.find(element => element.name == "Marco Lanci");
+
+console.log(students)
+console.log("classe dello studente Marco Lanci: ", MarcoLanci.class)
