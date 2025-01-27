12c672c38c04f0c6acc76088ee0b5563f6ba4534
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-27 12:21:54 +0100

    add: es. 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -21,3 +21,8 @@ const students = [
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
+
+const student2 = students.find(element => element.id == 2);
+
+console.log(students)
+console.log("stundente id 2: ", student2)
