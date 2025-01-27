2fbcc478f3958d768099580ef1604422bc8b52e6
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-01-27 12:06:46 +0100

    add: es. 3

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,12 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+// const numPlusOne = numbers.map(element => {
+//     return element + 1;
+// });
+
+const numPlusOne = numbers.map(element => element + 1);
+
+console.log(numbers)
+console.log("numeri + 1: ", numPlusOne)
